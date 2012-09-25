ecd918510e317d02f87a57d06ad0ed4afeaa0b96
ali <ali@rabe.(none)>
2012-09-25 18:18:51 +0200

    added table for averages and costs

--- js/main12.js
+++ js/main12.js
@@ -56,14 +56,13 @@ var counters = {
         color: "#dddddd",
         lines: {
             show: true,
-            //lineWidth : 1,
+            lineWidth : 1,
             stacked: false,
             fill: true,
+            fillOpacity : 0.2,
         },
         data: []
-    }
-/*
-*/
+    },
 };
 
 
@@ -76,50 +75,6 @@ function yTickFormatter(val, axisOpts) {
 }
        
 
-// draw a table with all rrd values
-function table_update() {
-    ds_idx=0;
-    rra_idx=9;
-    clm_nr=20;
-
-    // cleanup
-    // rows may have been added during previous updates
-    var oTable=document.getElementById("infotable");
-    while (oTable.rows.length>=4) {
-        oTable.deleteRow(3);
-    } 
-
-    $.each(rrd_data, function(idx, i_rrd_data) {
-        // Generic header info
-        oRow=oTable.insertRow(-1);
-        var oCell=oRow.insertCell(-1);
-        oCell.innerHTML=i_rrd_data.getDS(ds_idx).getName();;
-        oCell=oRow.insertCell(-1);
-        oCell.innerHTML=rra_idx;
-
-        // get RRA info
-        var rra=i_rrd_data.getRRA(rra_idx);
-        var rows=rra.getNrRows();
-
-        var oRow=undefined;
-        for (var i=0; i<rows;i++) {
-            if ((i%clm_nr)==0) {
-                // One new row every clm_nr
-                oRow=oTable.insertRow(-1);
-                var oCell=oRow.insertCell(-1);
-                oCell.innerHTML=i;
-            }
-            var oCell=oRow.insertCell(-1);
-            oCell.colSpan=20/clm_nr;
-            var el=rra.getElFast(i,ds_idx);
-            if (el!=undefined) {
-                oCell.innerHTML=Math.round(rra.getElFast(i,ds_idx));
-            } else {
-                oCell.innerHTML="-";
-            }
-        }
-    });
-}      
 
 function regionMap(seriesData, from, to, fn) {
     var res=[], rec;
@@ -141,6 +96,38 @@ function getSeriesAverage(seriesData, from, to) {
             return previousValue+currentValue;
         });
     return sum/values.length;
+}
+
+function getSeriesTotalConsumption(seriesData, from, to) {
+    var duration = to - from;
+    var avg = getSeriesAverage(seriesData, from, to);
+
+    return avg*duration / 3600;
+}
+
+function update_table(min, max) {
+    var oAvgTot=document.getElementById("sel_total_avg");
+    oAvgTot.innerHTML = Math.round(getSeriesAverage(counters.total.data, min, max)) + " W";
+
+    var oAvgC1=document.getElementById("sel_counter1_avg");
+    oAvgC1.innerHTML = Math.round(getSeriesAverage(counters.counter1.data, min, max)) + " W";
+
+    var oAvgC2=document.getElementById("sel_counter2_avg");
+    oAvgC2.innerHTML = Math.round(getSeriesAverage(counters.counter2.data, min, max)) + " W";
+
+    var oAvgC3=document.getElementById("sel_counter3_avg");
+    oAvgC3.innerHTML = Math.round(getSeriesAverage(counters.counter3.data, min, max)) + " W";
+
+    var oAvgLost=document.getElementById("sel_lost_avg");
+    oAvgLost.innerHTML = Math.round(getSeriesAverage(counters.lost.data, min, max)) + " W";
+
+    var oTotCons=document.getElementById("sel_total_consumption");
+    var consumption = getSeriesTotalConsumption(counters.total.data, min, max) / 1000;
+    oTotCons.innerHTML =consumption.toFixed(3) + " kWh";
+
+    var oTotCost=document.getElementById("sel_total_cost");
+    var cost = parseFloat(getSeriesTotalConsumption(counters.total.data, min, max) / 1000 * 0.23);
+    oTotCost.innerHTML = cost.toFixed(2) + " &euro;";
 }
 
 function draw_graph(container) {
@@ -175,8 +162,7 @@ function draw_graph(container) {
         // Clone the options, so the 'options' variable always keeps intact.
         o = Flotr._.extend(Flotr._.clone(options), opts || {});
 
-        oAvgCell=document.getElementById("sel_avg");
-        oAvgCell.innerHTML = "average: " + getSeriesAverage(counters.counter3.data, o.xaxis.min, o.xaxis.max);
+        update_table(o.xaxis.min, o.xaxis.max);
 
         // Return a new graph.
         return Flotr.draw(
@@ -204,8 +190,13 @@ function draw_graph(container) {
                 noTicks : cXaxisOpts.noTicks,
                 tickDecimals : cXaxisOpts.tickDecimals,
             },
-            yaxis : { min : area.y1, max : area.y2, autoscale : true },
-            bars : cBarOpts,
+            yaxis : {
+                min : area.y1,
+                max : area.y2,
+                autoscale : true,
+                tickFormatter : function(val, axisOpts) {return yTickFormatter(val, axisOpts);},
+            },
+           // bars : cBarOpts,
         });
     });
 
@@ -220,16 +211,21 @@ function draw_graph(container) {
 
 function prepare_rrd_data() {
 
+    var f_rows;
+
     oSelRRA=document.getElementById("select_rra");
     rraIdx=oSelRRA.options[oSelRRA.selectedIndex].value;
     if (!rraIdx) rraIdx = 9;
     console.log("Selected RRA: " + rraIdx);
+
+    counters.total.data = [];
 
     for (var dsIdx = 0; dsIdx < 4; dsIdx++) {
         // get RRA info
         var rra = rrd_data.getRRA(rraIdx);
         var ds_name = rrd_data.getDS(dsIdx).getName();
         var rows = rra.getNrRows();
+        f_rows = rows;
         var last_update = rrd_data.getLastUpdate();
         var step = rra.getStep();
         var ts = (last_update - (step * rows));
@@ -245,17 +241,39 @@ function prepare_rrd_data() {
 
         counters[(ds_name)].data = [];
 
-        var firstValueSeen = false;
         
-        for (var i = 0; i < rows; i++) {
+        for (var i = 0; i < f_rows; i++) {
             var el = rra.getElFast(i,dsIdx);
             if (i >= rows - 10) console.log(ds_name + ": " + ts + ": " + el * 1800);
-            if (!isNaN(el)) firstValueSeen = true;
-            if (isNaN(el)) el = 0;
-            if (firstValueSeen) counters[(ds_name)].data.push( [ ts, el * 1800 ] );
+            //if (!isNaN(el)) firstValueSeen = true;
+            if (isNaN(el)) { el = 0; }
+            //if (firstValueSeen) {
+                counters[(ds_name)].data.push( [ ts, el * 1800 ] );
+                if (counters.total.data[(i)]) {
+                    counters.total.data[(i)][1] += el * 1800;
+                } else {
+                    counters.total.data.push( [ ts, el * 1800 ] );
+                }
+            //}
             ts += (step);
         }
     }
+
+    // truncate empty beginnings of data series
+    for (var i = 0; i < f_rows; i++) {
+        if (counters.total.data[(i)][1] === 0) {
+            console.log("deleting datapoint " + counters.total.data[(i)][0]);
+            counters.counter1.data.shift();
+            counters.counter2.data.shift();
+            counters.counter3.data.shift();
+            counters.lost.data.shift();
+            counters.total.data.shift();
+        } else {
+            break;
+        }
+    }
+/*
+*/
 
 
     // this function is invoked when the RRD file name changes
@@ -263,8 +281,8 @@ function prepare_rrd_data() {
     // First clean up anything in the element
     //while (base_el.lastChild!=null) base_el.removeChild(base_el.lastChild);
     var mygraph = document.getElementById("mygraph");
-    console.log("Cleaning up canvas...");
-    while (mygraph.hasChildNodes()) mygraph.removeChild(mygraph.firstChild);
+    //console.log("Cleaning up canvas...");
+    //while (mygraph.hasChildNodes()) mygraph.removeChild(mygraph.firstChild);
     console.log("Drawing chart...");
     draw_graph(mygraph);
 
@@ -299,11 +317,5 @@ try {
     alert("Failed loading " + rrd_file + "\n" + err);
 }
 
-
-
-// html stuff..
-
-// Remove the Javascript warning
-document.getElementById("infotable").deleteRow(0);
 
 // vim: expandtab sw=4 ts=4
