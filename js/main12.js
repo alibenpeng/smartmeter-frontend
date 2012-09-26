76701d4add81145dcd24c8c555eddd2cb11b1852
ali <ali@rabe.(none)>
2012-09-26 11:49:46 +0200

    included spinner code

--- js/main12.js
+++ js/main12.js
@@ -22,6 +22,8 @@ var cBarOpts = {
 
 var cMouseOpts = {
     track           : true, // Enable mouse tracking
+    trackAll        : true,
+    trackY          : true,
     lineColor       : 'purple',
     relative        : true,
     position        : 'nw',
@@ -100,6 +102,25 @@ var counters = {
         data: []
     },
 };
+
+var cSpinnerOpts = {
+    lines: 9, // The number of lines to draw
+    length: 7, // The length of each line
+    width: 4, // The line thickness
+    radius: 10, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    color: '#000', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+};
+
 
 function objDump(arr,level) {
     var dumped_text = "";
@@ -364,6 +385,8 @@ function update_fname_handler(bf) {
 
 // load rrd file and give it to the preprocessor
 console.log("Fetching " + rrd_file);
+var spinnerTarget = document.getElementById("mygraph");
+var spinner = new Spinner(cSpinnerOpts).spin(spinnerTarget);
 try {
     FetchBinaryURLAsync(rrd_file,update_fname_handler);
 } catch (err) {
